48381c453e05c141191c3a45398f054a816bc5b9
JustTosYa1 <d9263466376@gmail.com>
2025-04-06 21:29:38 +0300

    commit

--- main.py
+++ main.py
@@ -1,74 +1,104 @@
+# coding: utf-8
+# Импортирует поддержку UTF-8.
 from __future__ import unicode_literals
+
+# Импортируем модули для работы с JSON и логами.
 import json
 import logging
+
+# Импортируем подмодули Flask для запуска веб-сервиса.
 from flask import Flask, request
+application = Flask(__name__)
 
-app = Flask(__name__)
+
 logging.basicConfig(level=logging.DEBUG)
-session_store = {}
 
+# Хранилище данных о сессиях.
+sessionStorage = {}
 
-@app.route("/", methods=["POST"])
+# Задаем параметры приложения Flask.
+@application.route("/", methods=['POST'])
+
 def main():
-    req_json = request.json
-    logging.info("Request: %r", req_json)
-    res_json = {
-        "version": req_json["version"],
-        "session": req_json["session"],
-        "response": {"end_session": False},
-    }
-    process_dialog(req_json, res_json)
-    logging.info("Response: %r", res_json)
-    return json.dumps(res_json, ensure_ascii=False, indent=2)
+# Функция получает тело запроса и возвращает ответ.
+    logging.info('Request: %r', request.json)
 
-
-def process_dialog(req, res):
-    uid = req["session"]["user_id"]
-    # Инициализируем сессию: round 1 - покупка слона, round 2 - покупка кролика.
-    if req["session"]["new"]:
-        session_store[uid] = {
-            "options": ["Не хочу.", "Не буду.", "Отстань!"],
-            "round": 1
+    response = {
+        "version": request.json['version'],
+        "session": request.json['session'],
+        "response": {
+            "end_session": False
         }
-        res["response"]["text"] = "Привет! Купи слона!"
-        res["response"]["buttons"] = generate_buttons(uid)
+    }
+
+    handle_dialog(request.json, response)
+
+    logging.info('Response: %r', response)
+
+    return json.dumps(
+        response,
+        ensure_ascii=False,
+        indent=2
+    )
+
+# Функция для непосредственной обработки диалога.
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+
+    if req['session']['new']:
+        # Это новый пользователь.
+        # Инициализируем сессию и поприветствуем его.
+
+        sessionStorage[user_id] = {
+            'suggests': [
+                "Не хочу.",
+                "Не буду.",
+                "Отстань!",
+            ]
+        }
+
+        res['response']['text'] = 'Привет! Купи слона!'
+        res['response']['buttons'] = get_suggests(user_id)
         return
 
-    utterance = req["request"]["original_utterance"].lower()
-    key_phrases = ["ладно", "куплю", "покупаю", "хорошо"]
-    current_round = session_store[uid].get("round", 1)
-
-    if any(phrase in utterance for phrase in key_phrases):
-        if current_round == 1:
-            res["response"]["text"] = "Поздравляем, вы купили слона! А теперь купите кролика!"
-            session_store[uid]["round"] = 2
-            session_store[uid]["options"] = ["Не хочу.", "Не буду.", "Отстань!"]
-            res["response"]["buttons"] = generate_buttons(uid)
-        else:
-            res["response"]["text"] = "Поздравляем, вы купили кролика!"
+    # Обрабатываем ответ пользователя.
+    if req['request']['original_utterance'].lower() in [
+        'ладно',
+        'куплю',
+        'покупаю',
+        'хорошо',
+    ]:
+        # Пользователь согласился, прощаемся.
+        res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
         return
 
-    if current_round == 1:
-        res["response"]["text"] = 'Все говорят "%s", а ты купи слона!' % req["request"]["original_utterance"]
-    else:
-        res["response"]["text"] = 'Все говорят "%s", а ты купи кролика!' % req["request"]["original_utterance"]
-    res["response"]["buttons"] = generate_buttons(uid)
+    # Если нет, то убеждаем его купить слона!
+    res['response']['text'] = 'Все говорят "%s", а ты купи слона!' % (
+        req['request']['original_utterance']
+    )
+    res['response']['buttons'] = get_suggests(user_id)
 
+# Функция возвращает две подсказки для ответа.
+def get_suggests(user_id):
+    session = sessionStorage[user_id]
 
-def generate_buttons(user_id):
-    session = session_store[user_id]
-    buttons = [{"title": opt, "hide": True} for opt in session["options"][:2]]
-    session["options"] = session["options"][1:]
-    session_store[user_id] = session
-    if len(buttons) < 2:
-        buttons.append({
+    # Выбираем две первые подсказки из массива.
+    suggests = [
+        {'title': suggest, 'hide': True}
+        for suggest in session['suggests'][:2]
+    ]
+
+    # Убираем первую подсказку, чтобы подсказки менялись каждый раз.
+    session['suggests'] = session['suggests'][1:]
+    sessionStorage[user_id] = session
+
+    # Если осталась только одна подсказка, предлагаем подсказку
+    # со ссылкой на Яндекс.Маркет.
+    if len(suggests) < 2:
+        suggests.append({
             "title": "Ладно",
-            "url": "https://market.yandex.ru/search?text=слон" if session.get("round", 1) == 1 \
-                else "https://market.yandex.ru/search?text=кролик",
+            "url": "https://market.yandex.ru/search?text=слон",
             "hide": True
         })
-    return buttons
 
-
-if __name__ == "__main__":
-    app.run()
+    return suggests
